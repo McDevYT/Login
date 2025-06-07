5678002141ce6565451ee8139bbd6dd6a779dac3
McDevYT <97825634+McDevYT@users.noreply.github.com>
2025-06-07 14:24:29 +0200

    Add Login System using bcrypt and jwt

---
+++ backend/src/index.ts
@@ -0,0 +1,109 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { type User } from "./types";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { Request, Response, NextFunction } from "express";
+
+interface AuthenticatedRequest extends Request {
+  user?: { username: string }; // or whatever you put into the token
+}
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const users: User[] = [];
+const posts: { username: string; title: string }[] = [
+  {
+    username: "Kyle",
+    title: "Hehe",
+  },
+  {
+    username: "Bob",
+    title: "bobby",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/posts", authenticateToken, (req: AuthenticatedRequest, res) => {
+  res.json(posts.filter((post) => post.username === req.user?.username));
+});
+
+app.post("/users/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(hashedPassword);
+    const user: User = {
+      username: req.body.username,
+      password: hashedPassword,
+    };
+
+    users.push(user);
+    res.status(201).send();
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user: User | undefined = users.find(
+    (user) => user.username === username
+  );
+
+  if (!user) {
+    res.status(400).send("Cannot find user");
+    return;
+  }
+
+  try {
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = jwt.sign(
+        { username: username },
+        process.env.ACCESS_TOKEN_SECRET as string,
+        { expiresIn: "1h" }
+      );
+
+      res.json({ accessToken: accessToken });
+    } else {
+      res.send("Not allowed");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+function authenticateToken(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  const token = authHeader?.split(" ")[1];
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET as string,
+    (err: any, user: any) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    }
+  );
+}
+
+app.listen(3000);
